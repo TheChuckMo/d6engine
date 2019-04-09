11a10765aa7e0ad1c562534705f9d4c251a4b96b
Chuck Mo <chuck@moosejudge.com>
2019-04-09 00:53:25 -0700

    attribute update

--- d6engine/attribute.py
+++ d6engine/attribute.py
@@ -1,18 +1,32 @@
-from typing import Tuple, AnyStr
+from typing import NamedTuple, Tuple, AnyStr
+
+D6_ATTRIBUTE_MAX: int = 6
+D6_ATTRIBUTE_MIN: int = 1
+D6_ATTRIBUTE_DEFAULT: int = 3
 
 
 class D6Attribute(object):
-    label: str
-    _min: int = 1
-    _max: int = 6
-    _default: int = 3
+    label: AnyStr
     _value: int
-    _error_message: str
+    min: int
+    max: int
+    message: AnyStr
 
-    def __init__(self, label: str, **kwargs):
+    def __init__(self,
+                 label: str,
+                 value: int = D6_ATTRIBUTE_DEFAULT,
+                 max: int = D6_ATTRIBUTE_MAX,
+                 min: int = D6_ATTRIBUTE_MIN,
+                 message: AnyStr = None):
+        """D6 Attribute"""
         self.label = label
-        new_value: int = kwargs.get('value', self._default)
-        self.value = new_value
+        self.max = max
+        self.min = min
+
+        # min & max must be set before value
+        self.value = value
+
+        self.message = message
 
     def __repr__(self):
         return '{label}: {value}'.format(label=self.label, value=self._value)
@@ -24,11 +38,11 @@ class D6Attribute(object):
         return self.value
 
     def check_value(self, num: int) -> bool:
-        if num > self._max:
-            self._error_message = 'value {} is larger than max {}'.format(num, self._max)
+        if num > self.max:
+            self.message = 'value {} is larger than max {}'.format(num, self.max)
             return False
-        elif num < self._min:
-            self._error_message = 'value {} is smaller than min {}'.format(num, self._min)
+        elif num < self.min:
+            self.message = 'value {} is smaller than min {}'.format(num, self.min)
             return False
         else:
             return True
@@ -42,37 +56,69 @@ class D6Attribute(object):
         if self.check_value(num):
             self._value = num
         else:
-            raise TypeError(self._error_message)
+            raise TypeError(self.message)
 
     def __add__(self, num: int):
+        """add an integer
+
+        self.value
+        4
+        self = self + 1
+        self.value
+        5
+        """
         add_value: int = self._value + num
         if self.check_value(add_value):
             return D6Attribute(label=str(self), value=add_value)
         else:
-            raise TypeError(self._error_message)
+            raise TypeError(self.message)
 
     def __iadd__(self, num: int):
+        """add an integer
+
+        self.value
+        2
+        self += 1
+        self.value
+        3
+        """
         add_value: int = self._value + num
         if self.check_value(add_value):
             self._value = add_value
             return self
         else:
-            raise TypeError(self._error_message)
+            raise TypeError(self.message)
 
     def __sub__(self, num: int):
+        """subtract an integer
+
+        self.value
+        4
+        self = self - 1
+        self.value
+        3
+        """
         sub_value: int = self._value - num
         if self.check_value(sub_value):
             return D6Attribute(label=str(self), value=sub_value)
         else:
-            raise TypeError(self._error_message)
+            raise TypeError(self.message)
 
     def __isub__(self, num: int):
+        """subtract an integer
+
+        self.value
+        3
+        self -=  1
+        self.value
+        2
+        """
         sub_value: int = self._value - num
         if self.check_value(sub_value):
             self._value = sub_value
             return self
         else:
-            raise TypeError(self._error_message)
+            raise TypeError(self.message)
 
 
 class D6AttributeList(object):
---
+++ docs/d6_attributes.md
@@ -0,0 +1,41 @@
+# D6 Engine Attribute System
+
+### D6Attribute object
+
+```pythonstub
+from d6engine.attribute import D6Attribute, D6AttributeList
+strength = D6Attribute(label='Strength')
+wisdom = D6Attribute(label='Wisdom', value=2)
+strength.label
+'Strength'
+strength.value
+3
+str(wisdom)
+'Wisdom'
+int(wisdom)
+2
+strength = strength + 1
+strength.value
+4
+wisdom += 2
+int(wisdom)
+4
+```
+
+### D6AttributeList object
+
+```pythonstub
+from d6engine.attribute import D6Attribute, D6AttributeList
+attributes = D6AttributeList()
+attributes.strength.label
+'Strength'
+attributes.strength.value
+3
+str(attributes.willpower)
+'Willpower'
+int(attributes.willpower)
+3
+attributes.strength.value = 2
+attributes.strength.value
+2
+```
