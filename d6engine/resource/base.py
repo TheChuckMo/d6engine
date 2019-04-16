f6c6939546f1c5fd47f0d40fa67e3150b2c659f4
Chuck Mo <chuck@moosejudge.com>
2019-04-16 10:00:56 -0700

    steps

--- d6engine/resource/base.py
+++ d6engine/resource/base.py
@@ -1,5 +1,6 @@
 from collections import deque
 from dataclasses import dataclass, field, InitVar
+from collections.abc import Mapping
 from typing import Any
 
 from slugify import slugify
@@ -23,8 +24,8 @@ def default_verifier(value: [int, str], data: dict) -> bool:
 
 @dataclass(order=True, repr=False)
 class CharacterEntry:
-    entry: InitVar[int, str]
     label: str = field()
+    entry: InitVar[int, str]
     _value: [int, str] = field(init=False, repr=False, compare=False)
 
     def __post_init__(self, entry: [int, str]):
@@ -40,6 +41,9 @@ class CharacterEntry:
 
     def __int__(self):
         return int(self.value)
+
+    def __bytes__(self):
+        return str(self.value).encode('utf-8')
 
     @property
     def value(self) -> [int, str]:
@@ -86,95 +90,10 @@ class CharacterEntry:
         return '::'.join(self._message)
 
 
-@dataclass(order=True)
-class CharacterComponent:
-    items: list
-#
-# class D6CharacterComponent(object):
-#     """
-#
-#     """
-#     label: str
-#     description: str
-#
-#     def __init__(self, items: list = []):
-#         """D6 Character Component"""
-#         if items is None:
-#             return
-#
-#         for item in items:
-#             self.add_item(item)
-#
-#     def add_item(self, item: D6CharacterEntry):
-#         """
-#
-#         Parameters
-#         ----------
-#         item
-#         """
-#         setattr(self, item.name, item)
-#
-#     def del_item(self, item: D6CharacterEntry):
-#         """
-#
-#         Parameters
-#         ----------
-#         item
-#         """
-#         delattr(self, item.name)
-#
-#     @property
-#     def name(self) -> str:
-#         """
-#
-#         Returns
-#         -------
-#
-#         """
-#         return slugify(self.label)
-#
-#     def values(self) -> list:
-#         """
-#
-#         Returns
-#         -------
-#
-#         """
-#         return [getattr(x, 'value') for x in self.__dict__.values()]
-#
-#     def labels(self) -> list:
-#         """
-#
-#         Returns
-#         -------
-#
-#         """
-#         return [getattr(x, 'label') for x in self.__dict__.values()]
-#
-#     def names(self) -> list:
-#         """
-#
-#         Returns
-#         -------
-#
-#         """
-#         return [x for x in self.__dict__.keys()]
-#
-#     def __str__(self):
-#         return '{}'.format(','.join(self.names()))
-#
-#     def __int__(self):
-#         return len(self)
-#
-#     def __repr__(self):
-#         items_repr: list = [repr(x) for x in self.__dict__.values()]
-#         return '{}(items=[{}])'.format(self.__class__.__name__, ','.join(items_repr))
-#
-#     def __getitem__(self, key: str):
-#         return self.__dict__.get(key, None)
-#
-#     def __getattr__(self, key: str):
-#         return self.__dict__.get(key, None)
-#
-#     def __len__(self):
-#         return len(self.__dict__)
+class CharacterComponent(Mapping):
+    pass
+
+# @dataclass(order=True)
+# class CharacterComponent:
+#     items: list
+
